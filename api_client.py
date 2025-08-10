c42fb39ef20cc231db4fe4e017a5be9ebd23e0c2
vaka-sravanthi <vakasravanthi99@gmail.com>
2025-08-10 11:24:24 +0530

    APIClIENT

--- api_client.py
+++ api_client.py
@@ -49,3 +49,46 @@ class APIClient:
         except requests.exceptions.RequestException as e:
             logging.error(f" Error fetching data from API: {e}")
             return []
+        
+    def fetch_vaccine_data(self, country, start_date, end_date):
+        config = configparser.ConfigParser()
+        config.read('config.ini')
+        vaccine_url = config['api']['vaccine_url']
+
+        url = f"{vaccine_url}/{country}?lastdays=all"
+        try:
+            logging.info(f" Fetching vaccination data for {country} from API...")
+            response = requests.get(url)
+            response.raise_for_status()
+            data = response.json()
+
+            if 'timeline' not in data:
+                logging.warning(f" No vaccination timeline data found for {country}.")
+                return []
+
+            vaccinations = data['timeline']
+            start = datetime.strptime(start_date, "%Y-%m-%d")
+            end = datetime.strptime(end_date, "%Y-%m-%d")
+
+            filtered_data = []
+            for date_str, value in vaccinations.items():
+                try:
+                    date_obj = datetime.strptime(date_str, "%m/%d/%y")
+                except ValueError:
+                    logging.warning(f"Invalid date format: {date_str}")
+                    continue
+
+                if start <= date_obj <= end:
+                    filtered_data.append((
+                        country,
+                        date_obj.strftime("%Y-%m-%d"),
+                        int(value)
+                    ))
+
+            logging.info(f" Retrieved {len(filtered_data)} vaccination records for {country}")
+            return filtered_data
+
+        except requests.exceptions.RequestException as e:
+            logging.error(f" Error fetching vaccination data: {e}")
+            return []
+
